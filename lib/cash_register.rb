e9e01ca03d8b0a3730a4900f3692ab9ece82da0d
Felice Forby <feliceforby@gmail.com>
2018-05-09 23:19:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   end
   
   def apply_discount
-    if @discount = 0
+    if @discount == 0
       return "There is no discount to apply."
     else
       applied_discount = @total * (@discount/100.to_f)
